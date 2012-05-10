8967fd22ad862e18b6a0b845ab3943406afc9763
jason@lilypepper.com <jason@lilypepper.com@163f7b89-2317-48a4-f55b-6fc381734fbc>
2012-05-10 22:52:58 +0000

    Initial import.

---
+++ trunk/jaks-maven-plugin/src/main/java/com/googlecode/jsaf/maven/AbstractGenerateScriptsMojo.java
@@ -0,0 +1,178 @@
+package com.googlecode.jsaf.maven;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.StringUtils;
+
+import com.googlecode.jsaf.cli.AbstractCommand;
+import com.googlecode.jsaf.cli.JSAFMain;
+import com.googlecode.jsaf.common.io.StreamUtil;
+
+import fmpp.util.FileUtil;
+
+/**
+ * Generate launch scripts.
+ * @author Jason Smith
+ */
+public abstract class AbstractGenerateScriptsMojo extends AbstractJSAFMojo
+{
+	/**
+	 * The command launcher.
+	 * <br/><strong>Default</strong>: <tt>{@link JSAFMain}</tt>
+	 * @parameter
+	 */
+	protected final Class<?> launcher = JSAFMain.class;
+	
+	/**
+	 * Location of launch-scripts and any other executables.
+	 * <br/><strong>Alias</strong>:<tt>bin-folder</tt>
+	 * <br/><strong>Default</strong>: <tt>bin</tt>
+	 * @parameter alias="bin-folder"
+	 */
+	protected final String binFolder = "bin";
+
+	
+	protected abstract String getClassPath() throws Exception;
+	
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException 
+	{
+		try
+		{
+			final Map<String,Class<?>> commands = getCommands();
+			for(final String command : commands.keySet())
+			{
+				final Class<?> commandClass = commands.get(command);
+				final File script = new File(new File(getGeneratedAppFolder(), binFolder), adjustScriptFileNameToOS(command));
+				getLog().info(script.getPath());
+				script.getParentFile().mkdirs();
+				StreamUtil.transfer(
+						new StringReader(getScript(command, commandClass)), 
+						new OutputStreamWriter(new FileOutputStream(script), StreamUtil.UTF8));
+			}
+		}
+		catch(final Exception e)
+		{
+			throw new MojoExecutionException("I've lost me mojo! " + e.toString(), e);
+		}
+	}
+	
+	protected abstract String adjustScriptFileNameToOS(final String command);
+	
+	protected abstract String getScript(final String command, final Class<?> commandClass) throws Exception;
+	
+	protected String getRelPathFromBinToLib() throws IOException
+	{
+		File lib = new File(getGeneratedAppFolder(), libFolder);
+		File bin = new File(getGeneratedAppFolder(), binFolder);
+		return FileUtil.getRelativePath(bin, lib);
+	}
+	
+	protected Map<String,Class<?>> getCommands() throws Exception
+	{
+		final Map<String,List<Class<?>>> rawCommands = new HashMap<String,List<Class<?>>>();
+		for(final Class<?> commandClass : getCommandClasses())
+		{
+			final String name = getCommandName(commandClass);
+			if(!rawCommands.containsKey(name))
+			{
+				rawCommands.put(name, new ArrayList<Class<?>>());
+			}
+			rawCommands.get(name).add(commandClass);
+		}
+		
+		for(final List<Class<?>> classes : rawCommands.values())
+		{
+			trimCommands(classes);
+		}
+		
+		final Map<String,Class<?>> results = new TreeMap<String,Class<?>>();
+		for(final String name : rawCommands.keySet())
+		{
+			for(final Class<?> commandClass : rawCommands.get(name))
+			{
+				String adjustedName = name;
+				int count = 0;
+				while(results.containsKey(adjustedName))
+				{
+					adjustedName = name + "-" + count;
+					count++;
+				}
+				results.put(adjustedName, commandClass);
+			}
+		}
+		
+		return results;
+	}
+
+	private List<Class<?>> getCommandClasses() throws IOException, ClassNotFoundException
+	{
+		final List<URL> urls = new ArrayList<URL>();
+		for(final Artifact artifact : getCompileArtifacts())
+		{
+			urls.add(artifact.getFile().toURI().toURL());
+		}
+		final URLClassLoader cl = new URLClassLoader(urls.toArray(new URL[urls.size()]), Thread.currentThread().getContextClassLoader());
+		
+		final List<Class<?>> commandClasses = new ArrayList<Class<?>>();
+		for(final URL commandClassDocument : Collections.list(cl.getResources("META-INF/jsaf/command-classes.txt")))
+		{
+			try(final LineNumberReader reader = new LineNumberReader(new InputStreamReader(commandClassDocument.openStream(), StreamUtil.UTF8)))
+			{
+				for(;;)
+				{
+					final String line = reader.readLine();
+					if(line == null)
+					{
+						break;
+					}
+					if(!StringUtils.isEmpty(line))
+					{
+						commandClasses.add(Class.forName(line, true, cl));
+					}
+				}
+			}
+		}
+		
+		return commandClasses; 
+	}
+	
+	private String getCommandName(final Class<?> commandClass) throws InstantiationException, IllegalAccessException
+	{
+		final AbstractCommand command = (AbstractCommand)commandClass.newInstance();
+		return command.getCommandName();
+	}
+	
+	
+	private void trimCommands(final List<Class<?>> commandClasses)
+	{
+		final List<Class<?>> copy = new ArrayList<Class<?>>(commandClasses);
+		for(final Class<?> a : copy)
+		{
+			for(final Class<?> b : copy)
+			{
+				if(!a.getCanonicalName().equals(b.getCanonicalName()) && a.isAssignableFrom(b))
+				{
+					commandClasses.remove(a);
+				}
+			}
+		}
+	}
+}
---
+++ trunk/jaks-maven-plugin/src/main/java/com/googlecode/jsaf/maven/GenerateWindowsScriptsMojo.java
@@ -0,0 +1,68 @@
+package com.googlecode.jsaf.maven;
+
+import java.util.Map;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.StringUtils;
+
+/**
+ * Generate Windows BAT launch scripts for JSAF commands.
+ * @author Jason Smith
+ * @requiresDependencyResolution compile
+ * @goal generate-windows-scripts
+ * @phase package
+ */
+public class GenerateWindowsScriptsMojo extends AbstractGenerateScriptsMojo
+{
+	@Override
+	protected String getScript(final String command, final Class<?> commandClass) throws Exception 
+	{
+		return "@echo off \r\n" +
+				"java.exe cp " + getClassPath() + " " + 
+				"-Djava.awt.headless=true " +
+				"-Djava.net.preferIPv4Stack=true " + 
+				"-XX:-OmitStackTraceInFastThrow " + 
+				quote("-Dmaven.groupId=" + project.getGroupId()) + " " + 
+				quote("-Dmaven.artifactId=" + project.getArtifactId()) + " " +
+				quote("-Dmaven.version=" + project.getVersion()) + " " +
+				"-Djsaf.ui.cols=0 " + 
+				quote("-Djsaf.launch.script=%~f0") + " " +
+				quote("-Djsaf.command.class=" + commandClass.getName()) + " " +
+				launcher.getName() + " " +
+				"%* \r\n" +
+				"SET errlev=%ERRORLEVEL% \r\n" +
+				"cmd /C exit /B %errlev% \r\n";
+	}
+	
+	@Override
+	protected String getClassPath() throws Exception 
+	{
+		final StringBuilder s = new StringBuilder();
+		s.append('"');
+		for(final Artifact artifact : getCompileArtifacts())
+		{
+			if(s.length() > 1)
+			{
+				s.append(';');
+			}
+			s.append("%~dp0\\");
+			s.append(this.getRelPathFromBinToLib().replace("/", "\\"));
+			s.append("\\" + artifact.getArtifactId() + "-" + artifact.getVersion() + ".jar");
+		}
+		s.append('"');
+		return s.toString();
+	}
+	
+	protected String quote(final String text)
+	{
+		return "\"" + text.replace("\"", "^\"") + "\"";
+	}
+
+	@Override
+	protected String adjustScriptFileNameToOS(String command) 
+	{
+		return command + ".bat";
+	}
+}
