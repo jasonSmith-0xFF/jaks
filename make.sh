c1434f5f09600963f4d196ef29d993bff26be979
jason <jason@lilypepper.com>
2012-05-11 00:45:06 +0000

    refactor

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@
 mvn clean install \
 && rm -rf $HOME/apps/jaks \
 && mkdir $HOME/apps/jaks \
-&& tar --directory "$HOME/apps/jaks" -xf "$(dirname 0)/jaks-examples/target/jaks-examples-0.0.1-SNAPSHOT.tar.gz"
+&& tar --directory "$HOME/apps/jaks" -xf "$(dirname 0)/jaks-examples/target/jaks-examples-0.0.1-SNAPSHOT.tar"
