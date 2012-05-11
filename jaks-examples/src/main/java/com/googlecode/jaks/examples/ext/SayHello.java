09adbb731d7862faa0079dc3f39f24af9b2a707f
jason <jason@lilypepper.com>
2012-05-11 00:18:50 +0000

    refactor

--- jaks-examples/src/main/java/com/googlecode/jaks/examples/ext/SayHello.java
+++ jaks-examples/src/main/java/com/googlecode/jaks/examples/ext/SayHello.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.googlecode.jsaf.examples.ext;
+package com.googlecode.jaks.examples.ext;
 
 public class SayHello extends com.googlecode.jaks.examples.SayHello
 {
